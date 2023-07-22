5119813ffa3c67daf6cef4dc5aad487d50f5450e
Adriano Gil <adrianogil.san@gmail.com>
2023-07-21 22:20:55 -0400

    Added a better cli for find pairs

--- RandomStudy/python/find_factors_pairs/find_factors_pairs.py
+++ RandomStudy/python/find_factors_pairs/find_factors_pairs.py
@@ -1,8 +1,13 @@
+def is_int(val):
+    try:
+        int(val)
+        return True
+    except ValueError:
+        return False
 
 
 def find_factor_pairs(number_list, target):
 	pairs = []
-
 	factors = []
 
 	for i in number_list:
@@ -14,7 +19,32 @@ def find_factor_pairs(number_list, target):
 	return pairs
 
 if __name__ == '__main__':
-	number_list = [1, 2, 3, 4, 5, 6]
-	target = 12
+	import sys
+
+	if len(sys.argv) > 1:
+		number_list_str = sys.argv[1]
+		number_list = []
+
+		number_str = ''
+		for s in number_list_str:
+			if is_int(s):
+				number_str += s
+			else:
+				number_list.append(int(number_str))
+				number_str = ""
+		if number_str:
+			number_list.append(int(number_str))
+			number_str = ""
+
+	else:
+		number_list = [1, 2, 3, 4, 5, 6]
+	
+	if len(sys.argv) > 2:
+		target = int(sys.argv[2])
+	else:
+		target = 12		
+	
+	print(f"list: {number_list}")
+	print(f"target: {target}")
 	pairs = find_factor_pairs(number_list, target)
-	print(pairs)
+	print(f"Found the following pairs: {pairs}")
